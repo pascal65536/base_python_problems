5eca77fbeffc22fda7bcef9e6c86f6ca9d38b9a1
pascal65536 <pascal65536@gmail.com>
2024-11-26 17:59:04 +0700

    turtle

---
+++ tutle_graph/1.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+speed(1)
+pensize(3)
+
+forward(200)
+right(90)
+
+forward(200)
+right(90)
+
+forward(200)
+right(90)
+
+forward(200)
+right(90)
+
+done()
---
+++ tutle_graph/2.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+bgcolor("black")
+speed(0)
+pensize(2)
+pencolor('yellow')
+
+
+for i in range(10):
+    '''
+    for i in range(10):
+        turtle.circle(150)
+        radius=radius-4
+    '''
+    circle(150)
+    right(36)
+
+done()
